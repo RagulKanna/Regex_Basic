845c0270a269c3a3d67dd2d616bbebb25bf3173a
RagulKanna <ragulragi619@gmail.com>
2021-09-15 09:33:42 +0530

    Added:validating the Password with rules

--- src/main/java/Assignment/UserDetails.java
+++ src/main/java/Assignment/UserDetails.java
@@ -44,7 +44,7 @@ public class UserDetails extends Regex {
 
     public static void password() {
         System.out.println("\nEnter the password with following rules");
-        System.out.println("1. minimum 8 characters");
+        System.out.println("1. minimum 8 characters\n2. at least 1 uppercase\n3. at least 1 number\n4. at least 1 special character");
         password = scan.nextLine();
         Regex.password(password);
     }
